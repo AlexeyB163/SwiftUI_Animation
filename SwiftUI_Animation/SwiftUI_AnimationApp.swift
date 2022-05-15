ab3da93e53ee5c1705d5723a92df24030404dfde
AlexeyB163 <sourceaisa@mail.ru>
2022-05-15 23:05:02 +0400

    Initial Commit

---
+++ SwiftUI_Animation/SwiftUI_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AnimationApp.swift
+//  SwiftUI_Animation
+//
+//  Created by User on 15.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
